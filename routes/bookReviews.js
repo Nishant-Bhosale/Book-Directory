1fe167c8a3cde8e0db9f46e42cb23e359f2be13f
Nishant <nishantbhosale244@gmail.com>
2021-07-15 14:37:15 +0530

    Prevented User from posting review more than one time

--- routes/bookReviews.js
+++ routes/bookReviews.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const auth = require("../middleware/auth");
 const router = express.Router();
-const Book = require("../models/Book");
 const BookReview = require("../models/BookReview");
+
+//Get Reviews
+router.get("/books/review/:id", auth, async (req, res) => {
+	const _id = req.params.id;
+
+	try {
+		const reviews = await BookReview.find({ for: _id });
+
+		if (reviews.length <= 0) {
+			return res.status(400).json({ msg: "No Reviews exist" });
+		}
+
+		res.status(200).json(reviews);
+	} catch (error) {
+		console.log(error);
+		res.status(500).send();
+	}
+});
 
 //Add new Review
 router.post("/books/review/:id", auth, async (req, res) => {
@@ -10,6 +27,19 @@ router.post("/books/review/:id", auth, async (req, res) => {
 	const { review } = req.body;
 
 	try {
+		const checkBookReview = await BookReview.findOne({
+			for: _id,
+			reviewBy: req.user.name,
+		});
+
+		console.log(checkBookReview);
+
+		if (checkBookReview) {
+			return res
+				.status(400)
+				.json({ msg: "Cannot post review more than one time." });
+		}
+
 		const bookReview = new BookReview({
 			review,
 			for: _id,
@@ -17,7 +47,7 @@ router.post("/books/review/:id", auth, async (req, res) => {
 		});
 
 		await bookReview.save();
-		console.log(bookReview);
+
 		res.status(201).send(bookReview);
 	} catch (error) {
 		console.log(error);
